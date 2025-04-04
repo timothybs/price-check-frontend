597c0a059014b39a21ad266d52e07109d7b6679f
Tim Sumner <timothy@sumner.eu.org>
2025-04-04 22:46:33 +0100

    Testing

--- pages/matches.js
+++ pages/matches.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
-
-console.log('🔑 URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
-console.log('🔑 KEY:', process.env.NEXT_PUBLIC_SUPABASE_KEY)
-
+// Testing
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_KEY
