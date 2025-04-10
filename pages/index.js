9392c883dbb66ccb4b08c2551fd6fd96e1a19067
Tim Sumner <timothy@sumner.eu.org>
2025-04-10 16:52:21 +0100

    Update Index

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ export default function Home() {
       <ul>
         <li><a href="/matches">Weekly Order (HH-TB-ST)</a></li>
         <li><a href="/centurion-orders">Centurion Orders</a></li>
+        <li><a href="/wilsons-orders">Wilsons Orders</a></li>
+        <li><a href="/toolstream-orders">Toolstream Orders</a></li>
         <li><a href="/orders">Orders (WIP)</a></li>
         <li><a href="/compare">Compare (WIP)</a></li>
       </ul>
